91fbb1149cedd797416f204820e0cb10a4f949bd
Gabriel <gabrielbeltrando@gmail.com>
2023-10-06 08:42:47 -0300

    'PERN'

--- src/controllers/tareas.controllers.js
+++ src/controllers/tareas.controllers.js
@@ -1,8 +1,26 @@
+import {pool} from '../db.js';
+
 export const listarTareas = (req, res) => res.send('obteniendo tareas');
 
 export const listarTarea = (req, res) => res.send('obteniendo tarea unica');
 
-export const crearTarea = (req, res) => res.send('creando tareas');
+export const crearTarea = (req, res, next) => {
+    const { titulo, descripcion } = req.body;
+    
+    try {
+        throw new Error ('Algo salio mal');   
+        const rows = pool.query('INSERT INTO tareas (titulo, descripcion) VALUES ($1, $2)', [titulo, descripcion]);
+        console.log(rows);
+        res.send('Creando tarea');
+    }catch(error){
+        if (error.code === '23505') {
+            return res.send('Ya existe una tarea con ese titulo');
+        }
+        console.log(error);
+        next(error);
+    }
+    
+}
 
 export const actualizarTarea = (req, res) => res.send('actualizando tareas');
 
--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import app from "./app.js";
 
 app.listen(3000);
 
-console.log("El servidor en puerto", 3000);
+console.log("El servidor ON en puerto", 3000);
+
